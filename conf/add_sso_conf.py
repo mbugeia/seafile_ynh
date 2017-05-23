f40e1ef9687976b8824edd66fa8a1b33132954f8
Rafi59 <Rafi594@users.noreply.github.com>
2017-05-23 16:58:02 +0200

    Update ssowat conf

--- conf/add_sso_conf.py
+++ conf/add_sso_conf.py
@@ -2,11 +2,11 @@ import json
 
 with open("/etc/ssowat/conf.json.persistent", "r") as jsonFile:
     data = json.load(jsonFile)
-    if "unprotected_urls" in data:
-        data["unprotected_urls"].append("/seafhttp")
+    if "ignore_urls" in data:
+        data["ignore_urls"].append("/seafhttp")
     else:
-        data["unprotected_urls"] = ["/seafhttp"]
-    data["unprotected_urls"].append("/seafdav")
+        data["ignore_urls"] = ["/seafhttp"]
+    data["ignore_urls"].append("/seafdav")
 
 with open("/etc/ssowat/conf.json.persistent", "w") as jsonFile:
     jsonFile.write(json.dumps(data, indent=4, sort_keys=True))
